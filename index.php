5f4cf66deaea26f6bc5dcaaabc7b487d92da1437
aref-ivan <aref.ivan@gmail.com>
2020-06-01 01:02:57 +0300

    задание 2 done!

--- index.php
+++ index.php
@@ -2,42 +2,24 @@
 
     include_once("libs/functions.php");
     $str= file_get_contents("http://old.code.mu/exercises/advanced/php/parsing/parsing-sajtov-regulyarnymi-vyrazeniyami-php/2/1.php");
-    $regBody="#<body[^>]*>(.*)</body>#su";
-    $str1='href="\\\'test\'\\"';
-    file_put_contents("2.php",$str);
+    $body = searchTag($str,"body");
+    $bodyStr = implode($body[0]); 
 
-    $elemID="content";
-
-    $regParagraf='#<\s*?p\s*?[^>]*?>(.+?)<\s*?/p\s*?>#su';
-     $regClass = '#<\s*?(a|p|div)\s+?class\s*?=\s*.?www[^>]*?>#su';
-    $regRef='#href=(.)(.+?)\1#su';
-    $regID='#<\s*?([a-z]{3,})\s+?[^>]*?id\s*?=\s*?\"\s*?'.$elemID.'\s*?\"[^>]*?>(.+?)<\s*?/\1\s*?>\s*?<script#su';
-    preg_match_all($regClass,$str,$classArr);
-    var_dump($classArr[0]);
-    preg_match_all($regBody,$str,$bodyArr);
-    $body=$bodyArr[1][0]; //Нашли <body>
-
-    preg_match_all($regID,$body,$contentArr);
-    $content=$contentArr[2][0]; //нашли содержимое id="content"
-
-    preg_match_all($regParagraf,$body,$paragrafArrBody);
-    preg_match_all($regParagraf,$content,$paragrafArrContent);
-    $paragrafsBody=$paragrafArrBody[0];
-    // foreach ($paragrafArrBody[0] as $paragraf){
-    //     if(preg_match($regClass,$paragraf)){
-    //     echo $paragraf ."<br>";
-    //     }
-    //     else  echo 1 . "<br>";
-    // };
-    // var_dump($paragrafArr);
+    $contentId = searchId($bodyStr,"content","<script");
+    $contentStr= implode($contentId[0]);
+    $linkContent = searchLink($contentStr);
     
+    $paragrafs = implode(searchTag($str,"p")[0]);
+
+    $paragrafsClassWWW=searchClass($paragrafs,'www');
+    $linkClassWWWContents=searchLink(implode(searchClass(implode(searchTag($contentStr,"a")[0]),"www")[0]));//нашли все <a> в content, потом среди них нашли с классом www, и передали в массив
+    $pag=implode(searchClass($str,"pag")[0]);
+    $linksPag = searchLink($pag);
+    $linkActive=searchLink(implode(searchClass($pag,"active")[0]));
+    $footer = implode(searchId($str,"footer")[0]);
     
-    // echo $bodyContent;
-    // preg_match_all($regRef,$str,$refArr);
-    // searchLink($content); //нашли ссылки в content
-    // echo $str;
-//    foreach ($paragrafsBody as $paragraf){
-    //    echo $paragraf;}
+    $parClassEEEFooter = searchClass(implode(searchTag($footer,"p")[0]),"eee");
+    var_dump($parClassEEEFooter);
 
     
 ?>
--- libs/functions.php
+++ libs/functions.php
@@ -4,27 +4,55 @@ function searchLink($target){
     $regLink='#<\s*?a\s+?[^>]*?>(.+?)</a>#su';
     $regHref='#href\s*?=(.)(.+?)\1#';
     $regClassLink='#class\s*?=\s*?"(.+?)"\s*?#';
-    
+    $arr=[];
+    $link="";
+    $name="";
 
     
     preg_match_all($regLink,$target,$linkArr);
-    // echo $site;
+    
     foreach ($linkArr[0] as $key => $value){
-
-        preg_match($regHref,$value,$href);
-        
+        $name=$linkArr[1][$key];
+        preg_match($regHref,$value,$href);        
         $href[2]=preg_replace('#[\'"\s]#',"",$href[2]);
-
         preg_match($regClassLink,$value,$classLink);
+
+        if($classLink){
+            $class=$classLink[1];
+        }
+        else{
+            $class="";
+        }
+        ;
+
+        $arr[$key]=[$value,$href[2],$name,$class];
         
 
-        echo "href: " . $href[2] . "<br>";
-        if($classLink)
-            {echo "class: " . $classLink[1] . "<br>";}
-        else{echo "class: " . "отсутствует" . "<br>";};
-        echo "Название: " . $linkArr[1][$key] . "<br>";
+        // echo "href: " . $href[2] . "<br>";
+        // if($classLink)
+        //     {echo "class: " . $classLink[1] . "<br>";}
+        // else{echo "class: " . "отсутствует" . "<br>";};
+        // echo "Название: " . $linkArr[1][$key] . "<br>";
     }
     
+    return $arr;
+}
+
+function searchClass($str,$className,$endHook=""){
+    $regClass = '#<\s*?([a-z]{1,})\s+?[^>]*?class\s*?=\s*([^\s]{1,2})\s*?'.$className.'\s*?\2[^>]*?>(.+?)<\s*?/\1\s*?>\s*?'.$endHook.'#su';
+    preg_match_all($regClass,$str,$arr);
+    return $arr;
+}
+function searchId($str,$idName,$endHook=""){
+    $regID='#<\s*?([a-z]{3,})\s+?[^>]*?id\s*?=\s*?\"\s*?'.$idName.'\s*?\"[^>]*?>(.+?)<\s*?/\1\s*?>\s*?'.$endHook.'#su';
+    preg_match_all($regID,$str,$arr);
+    return $arr;
+}
+function searchTag($str,$tag,$endHook=""){
+    $regTag="#<".$tag."[^>]*>(.*?)</".$tag.">\s*?".$endHook."#su";
+    preg_match_all($regTag,$str,$arr);
+    return $arr;
 
 }
 ?>
+
