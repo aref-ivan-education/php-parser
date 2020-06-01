44c0f80e1faeab3abf9f04c23c19c07f0681bd04
aref-ivan <aref.ivan@gmail.com>
2020-06-01 23:29:54 +0300

    task3 done

--- index.php
+++ index.php
@@ -9,9 +9,27 @@
 
     $linksMenu =searchLink($menu,"marr");//ссылки из меню
     // var_dump($linksMenu);
-    $imgs = searchTag($task3,"img");
+    $imgs = searchImg($rost_cakes);
     $classMore = searchClass($task3,"more");
-    $classWrpMain= searchClass($rost_cakes,"wrapper--main","str","<script");
-    var_dump($classWrpMain);
+    // $classWrpMain= searchClass($rost_cakes,"wrapper--main","str","<script");
+   $content=searchId($task3,"content","str","<div\s+?id=\"foo");
+   $scriptSContent=searchTag($content,"script","arr");
+
+    $content=str_replace($scriptSContent[0]," ",$content);//удалили скрипты
+    $imgContent=searchImg($content,"marr");
+    $content=str_replace($imgContent[0],"",$content);//удалили картинки
+    $classMoreContent = searchClass($content,"more","arr");
+    $content=str_replace($classMoreContent[0],"",$content);//удалили more
+    $contentH2=searchTag($content,"h2","arr")[0];
+    $linkH2 = searchLink(implode($contentH2),"marr");
+    $contentH2NoLinks=str_replace($linkH2[0],$linkH2[1],$contentH2);
+
+    $content=str_replace($contentH2,$contentH2NoLinks,$content);
+    $content = preg_replace("#<\s*?p[^>]*?>#su","<p>",$content);
+    // var_dump($content);
+
+    $content = preg_replace('#<\s*?span[^>]*?>|<\s*?/\s*?span[^>]*?>#su',"",$content);
+    var_dump($content);
+    
      
 ?>
--- libs/functions.php
+++ libs/functions.php
@@ -10,7 +10,8 @@ function searchLink($target,$type="arr"){
     preg_match_all($regLink,$target,$linkArr);  
     foreach ($linkArr[0] as $key => $value){
         $name=$linkArr[1][$key];
-        preg_match($regHref,$value,$href);        
+        preg_match($regHref,$value,$href);   
+        if(empty($href[2])){$href[2]="";}     
         $href[2]=preg_replace('#[\'"\s]#',"",$href[2]);
         ;
         $arr[$key]=[$value,$href[2],$name];
@@ -34,38 +35,68 @@ function searchLink($target,$type="arr"){
    
 }
 
-function searchImg($target){
+function searchImg($target,$type="arr"){
     $regImg="#<\s*?img\s+?[^>]*?>#su";
     $regSrc = '#src\s*?=\s*?([^\s]{1,2})\s*?(.+?)\1#';
+    $regAlt = '#alt\s*?=\s*?([^\s]{1,2})\s*?(.+?)\1#';
+    $arr=[];
+
+    preg_match_all($regImg,$target,$imgArr);
+
+    foreach($imgArr[0] as $key=> $value){
+        preg_match($regSrc,$value,$src);
+        preg_match($regAlt,$value,$alt);
+        if(!$alt){$alt[2]="";}
+        $arr[$key]=[$value,$src[2],$alt[2]];
+
+    }
+
+    switch($type){
+        case "arr":
+            return $arr;
+            break;
+        case "str": 
+            return implode($imgArr[0]);
+            break;
+        case "marr": 
+            return $imgArr;
+            break;
+        default:
+            return $arr;
+            break;
+
+    }
+
+
 
 }
 
 function searchClass($str,$className,$type="str",$endHook=""){
     $regClass = '#<\s*?([a-z]{1,})\s+?[^>]*?class\s*?=\s*([^\s]{1,2})[\sa-z0-9\-_]*?'.$className.'[\sa-z0-9\-_]*?\2[^>]*?>(.+?)<\s*?/\1\s*?>\s*?'.$endHook.'#su';
     preg_match_all($regClass,$str,$arr);
-    if(!$type=="str"){
-        return $arr;
-    }else{
+    if($type=="str"){
         return implode($arr[0]);
+    }else{
+       return $arr;
     }
     
 }
 function searchId($str,$idName,$type="str",$endHook=""){
     $regID='#<\s*?([a-z]{3,})\s+?[^>]*?id\s*?=\s*?\"\s*?'.$idName.'\s*?\"[^>]*?>(.+?)<\s*?/\1\s*?>\s*?'.$endHook.'#su';
     preg_match_all($regID,$str,$arr);
-    if(!$type=="str"){
-        return $arr;
-    }else{
+    if($type=="str"){
         return implode($arr[0]);
+    }else{
+       return $arr;
     }
 }
 function searchTag($str,$tag,$type="str",$endHook=""){
     $regTag="#<".$tag."[^>]*>(.*?)</".$tag.">\s*?".$endHook."#su";
     preg_match_all($regTag,$str,$arr);
-    if(!$type=="str"){
-        return $arr;
-    }else{
+    if($type=="str"){
         return implode($arr[0]);
+    }else{
+       return $arr;
     }
 
 }
