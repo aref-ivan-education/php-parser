8ed5d484f9421bb61d4ae8063c50d6b85224dbd4
aref-ivan <aref.ivan@gmail.com>
2020-06-02 11:06:12 +0300

    sites save

--- index.php
+++ index.php
@@ -1,26 +1,31 @@
 <?php
 include_once("libs/functions.php");
-	//Устанавливаем доступы к базе данных:
-		$host = 'localhost'; //имя хоста, на локальном компьютере это localhost
-		$user = 'root'; //имя пользователя, по умолчанию это root
-		$password = ''; //пароль, по умолчанию пустой
-		$db_name = 'parsesite'; //имя базы данных
+	// //Устанавливаем доступы к базе данных:
+	// 	$host = 'localhost'; //имя хоста, на локальном компьютере это localhost
+	// 	$user = 'root'; //имя пользователя, по умолчанию это root
+	// 	$password = ''; //пароль, по умолчанию пустой
+	// 	$db_name = 'parsesite'; //имя базы данных
 
-	//Соединяемся с базой данных используя наши доступы:
-        $link = mysqli_connect($host, $user, $password, $db_name);
-        mysqli_query($link, "SET NAMES 'utf8'")
-            or die( mysqli_error($link) );
+	// //Соединяемся с базой данных используя наши доступы:
+    //     $link = mysqli_connect($host, $user, $password, $db_name);
+    //     mysqli_query($link, "SET NAMES 'utf8'")
+    //         or die( mysqli_error($link) );
 
-	/*
-		Соединение записывается в переменную $link,
-		которая используется дальше для работы mysqi_query.
-    */
-    $table='1_task_sites';
-	$result = mysqli_query($link, "SELECT ref FROM ".$table." WHERE id>0");
+	// /*
+	// 	Соединение записывается в переменную $link,
+	// 	которая используется дальше для работы mysqi_query.
+    // */
+    // $table='1_task_sites';
+	// $result = mysqli_query($link, "SELECT ref FROM ".$table." WHERE id>0");
 	
 
-	for( $sites=[]; $row=mysqli_fetch_assoc($result); $sites[]=$row["ref"]);
+	// for( $sites=[]; $row=mysqli_fetch_assoc($result); $sites[]=$row["ref"]);
 	
+	// file_put_contents("sites.txt",implode("??",$sites));
+	$sites=explode("??",file_get_contents("sites.txt"));
+
+	var_dump($sites);
+
 	
 
    
