dd095463ddb545b6f378f9755498bb4f3db300e2
aref-ivan <aref.ivan@gmail.com>
2020-06-02 11:00:12 +0300

    curl начало

--- index.php
+++ index.php
@@ -1,35 +1,27 @@
 <?php
+include_once("libs/functions.php");
+	//Устанавливаем доступы к базе данных:
+		$host = 'localhost'; //имя хоста, на локальном компьютере это localhost
+		$user = 'root'; //имя пользователя, по умолчанию это root
+		$password = ''; //пароль, по умолчанию пустой
+		$db_name = 'parsesite'; //имя базы данных
 
-    include_once("libs/functions.php");
-    $rost_cakes = file_get_contents("rostov-cakes.html");
-    $task2= file_get_contents("http://old.code.mu/exercises/advanced/php/parsing/parsing-sajtov-regulyarnymi-vyrazeniyami-php/2/1.php");
-    $task3= file_get_contents("http://old.code.mu/exercises/advanced/php/parsing/parsing-sajtov-regulyarnymi-vyrazeniyami-php/3/1.php");
+	//Соединяемся с базой данных используя наши доступы:
+        $link = mysqli_connect($host, $user, $password, $db_name);
+        mysqli_query($link, "SET NAMES 'utf8'")
+            or die( mysqli_error($link) );
 
-    $menu = searchId($task3,"menu");
+	/*
+		Соединение записывается в переменную $link,
+		которая используется дальше для работы mysqi_query.
+    */
+    $table='1_task_sites';
+	$result = mysqli_query($link, "SELECT ref FROM ".$table." WHERE id>0");
+	
 
-    $linksMenu =searchLink($menu,"marr");//ссылки из меню
-    // var_dump($linksMenu);
-    $imgs = searchImg($rost_cakes);
-    $classMore = searchClass($task3,"more");
-    // $classWrpMain= searchClass($rost_cakes,"wrapper--main","str","<script");
-   $content=searchId($task3,"content","str","<div\s+?id=\"foo");
-   $scriptSContent=searchTag($content,"script","arr");
+	for( $sites=[]; $row=mysqli_fetch_assoc($result); $sites[]=$row["ref"]);
+	
+	
 
-    $content=str_replace($scriptSContent[0]," ",$content);//удалили скрипты
-    $imgContent=searchImg($content,"marr");
-    $content=str_replace($imgContent[0],"",$content);//удалили картинки
-    $classMoreContent = searchClass($content,"more","arr");
-    $content=str_replace($classMoreContent[0],"",$content);//удалили more
-    $contentH2=searchTag($content,"h2","arr")[0];
-    $linkH2 = searchLink(implode($contentH2),"marr");
-    $contentH2NoLinks=str_replace($linkH2[0],$linkH2[1],$contentH2);
-
-    $content=str_replace($contentH2,$contentH2NoLinks,$content);
-    $content = preg_replace("#<\s*?p[^>]*?>#su","<p>",$content);
-    // var_dump($content);
-
-    $content = preg_replace('#<\s*?span[^>]*?>|<\s*?/\s*?span[^>]*?>#su',"",$content);
-    var_dump($content);
-    
-     
-?>
+   
+?>  
